862bab201a96d732f25efd99632cdc65a3c5bc54
SAIGNE David <david.saigne@etu.univ-amu.fr>
2017-12-05 11:50:15 +0100

    footer added

---
+++ views/shared/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <span class="row">
+        <p>Site crée par David SAIGNE dans le cadre du cours Web LP SIL DA2I d'Aix-en-Provence<p>
+        <aside>But purement éducationnel</aside>
+    </span>
+</footer>
