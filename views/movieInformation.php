cc2fc909adeefb33199ca0fd4d2203c6fa85718b
SAIGNE David <david.saigne@etu.univ-amu.fr>
2017-12-05 11:48:07 +0100

    add realisator to movie information

--- views/movieInformation.php
+++ views/movieInformation.php
@@ -31,12 +31,29 @@
                     <?php foreach($data['pictureactor'] as $key => $value) { ?>
 
                     <figure class="col-4">
-                        <img src=<?=$value['path']?> alt="Actor">
+                        <img class="personPicture" src=<?=$value['path']?> alt="Actor">
                         <figcaption><?=$value['legend']?></figcaption>
                     </figure>
 
                     <?php } // end foreach ?> 
                 </section>
+
+                <!-- Realisateur -->
+                <section class="row" id="realisateur">
+                    <figure class="col-md-12 text-center">
+                        <img class="personPicture" src=<?= $data['picturedirector']['path'] ?> alt="realisateur" height="200px">
+                        <figcaption><?= $data['picturedirector']['legend'] ?></figcaption>
+                    </figure>
+                </section>
+
+                <!-- Images film -->
+                <section class="row">
+                    <h2>Images du film</h2>
+                    <?php //Controller::loadtemplate('imageList', $data); ?>
+                </section>
+
             </article>
+
+            <?php Controller::loadSharedTemplate('footer', $data); ?>
         </main>
     </body>
