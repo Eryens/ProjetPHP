1a0b0006f18bc66c4664e2734bd68ae5d3064eee
SAIGNE David <david.saigne@etu.univ-amu.fr>
2017-12-11 14:45:33 +0100

    ajout get picutre

---
+++ controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php 
+
+class HomeController extends Controller {
+    /**
+     * Displays the home page of the app
+     */
+    public static function displayHome()
+    {
+        //$data['moviesList'] = Movie::getAllMovies();
+        //$data['actorsList'] = Actor::getAllActors();
+        //$data['directorsList'] = Director::getAllDirectors();
+        return parent::loadTemplate('home', $data);
+    }
+}
--- models/Movie.php
+++ models/Movie.php
@@ -17,4 +17,30 @@ class Movie {
         $this->db = DB::getInstance();
     }
     
+    public function getMovieInformation() 
+    {
+        $request = $this->db->prepare("SELECT * FROM MOVIE WHERE id = :id");
+        $request->execute([':id' => $this->id,
+        ]);
+
+        return $request->fetchAll(PDO::FETCH_ASSOC)[0];
+    }
+
+    public static function getAllMovies() 
+    {
+        $db = DB::getInstance();
+
+        $request = $db->prepare("SELECT * FROM MOVIE ORDER BY TITLE ASC");
+        $request -> execute();
+
+        return $request->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getPictures() 
+    {
+        $db = DB::getInstance();
+        // redo it properly with DB
+        $request = $db->prepare("SELECT * FROM PICTURE JOIN MOVIE ON MOVIE_HAS_PICTURE ON PICTURE.ID = MOVIE_HAS_PHOTO.ID")
+    }
+
 }
